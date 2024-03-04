dc13d34b43fae50f769fbe7a406a07a90d685a7e
garnikx <garniktester@gmail.com>
2024-03-04 15:34:17 +0300

    add try/catch

--- src/main/java/com/example/First_Mock/FirstMockApplication.java
+++ src/main/java/com/example/First_Mock/FirstMockApplication.java
@@ -12,14 +12,14 @@ import org.springframework.context.annotation.Primary;
 @SpringBootApplication
 public class FirstMockApplication {
 
-	@Bean
-	@Primary
-	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-		mapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, true);
-		return mapper;
-	}
+//	@Bean
+//	@Primary
+//	public ObjectMapper objectMapper() {
+//		ObjectMapper mapper = new ObjectMapper();
+//		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+//		mapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, true);
+//		return mapper;
+//	}
 
 
 	public static void main(String[] args) {
--- src/main/java/com/example/First_Mock/RestApiController/RestApiController.java
+++ src/main/java/com/example/First_Mock/RestApiController/RestApiController.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
 public class RestApiController {
@@ -14,11 +17,24 @@ public class RestApiController {
         return ResponseEntity.ok(new User("aaa", "bbb"));
     }
 
-
     @PostMapping(value = "/postUser", consumes = {"*/*"})
-    ResponseEntity<?> Authenticate(@Validated @RequestBody User user) {
-        if ((user.getLogin() == null) || (user.getPassword() == null))
-            return ResponseEntity.internalServerError().build();
-        return ResponseEntity.ok(new User(user.getLogin(),user.getPassword()));
+    ResponseEntity<?> Authenticate(@RequestBody Map<String, String> request) {
+        try {
+            if ((request.size() > 2) || (!request.containsKey("login")) || (!request.containsKey("password")) || (request.get("login") == null) || (request.get("password") == null)) {
+                throw new Exception("Bad JSON!");
+            }
+            User user = new User(request.get("login"), request.get("password"));
+            return ResponseEntity.ok(user.toString());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
+
+
+//    @PostMapping(value = "/postUser", consumes = {"*/*"})
+//    ResponseEntity<?> Authenticate(@Validated @RequestBody User user) {
+//        if ((user.getLogin() == null) || (user.getPassword() == null))
+//            return ResponseEntity.internalServerError().build();
+//        return ResponseEntity.ok(new User(user.getLogin(),user.getPassword()));
+//    }
 }
